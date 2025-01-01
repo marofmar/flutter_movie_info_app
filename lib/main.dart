acaa00318f8190940d08cd4fd63597346dfdfa77
Yujin <yujin.mar.chung@gmail.com>
2025-01-01 21:26:14 +0900

    feat: 메인 함수에 dotenv 추가,

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_info_app/presentation/pages/home/movie_list_page.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MyApp()));
 }
 
